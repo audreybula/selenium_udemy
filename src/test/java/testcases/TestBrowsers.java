6d081adde00784b8a6c4f90a158255b5b33235a1
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-14 15:14:15 +1200

    automate with webdriver

--- src/test/java/testcases/TestBrowsers.java
+++ src/test/java/testcases/TestBrowsers.java
@@ -2,11 +2,18 @@ package testcases;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestBrowsers {
 	
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.gecko.driver", "/Users/admin/Desktop/capmation/selenium/drivers/geckodriver");
+//		System.setProperty("webdriver.gecko.driver", "/Users/admin/Desktop/capmation/selenium/drivers/geckodriver");
+		
+		// the webdriver manager automatically gets
+		// the drivers. for chrome, safari,
+		// change firefoxdriver to chromedriver / safaridriver
+		WebDriverManager.firefoxdriver().setup();
 		FirefoxDriver driver = new FirefoxDriver();
 		driver.get("http://google.com");
 		
