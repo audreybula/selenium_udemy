7d048a97b099b745a6ad8f4d6d9162c4e674820d
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-15 09:21:32 +1200

    WebDriver is a parent of individual drivers

--- src/test/java/testcases/TestBrowsers.java
+++ src/test/java/testcases/TestBrowsers.java
@@ -1,5 +1,7 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,9 +15,14 @@ public class TestBrowsers {
 		// the webdriver manager automatically gets
 		// the drivers. for chrome, safari,
 		// change firefoxdriver to chromedriver / safaridriver
-		WebDriverManager.firefoxdriver().setup();
-		FirefoxDriver driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+//		FirefoxDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get("http://google.com");
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.close(); // close the current browser
+		//driver.quit(); // close current browser + all related browsers
 		
 	}
 
