ec4ffe1df47b0946193af9509a6aa63f0278bdf6
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-16 12:18:50 +1200

    WebDriver interface

--- src/test/java/testcases/TestBrowsers.java
+++ src/test/java/testcases/TestBrowsers.java
@@ -3,21 +3,32 @@ package testcases;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBrowsers {
 	
+	// this is usually read from an excel sheet
+	public static String browser = "chrome";
+	public static WebDriver driver;
+	
 	public static void main(String[] args) {
 		
 //		System.setProperty("webdriver.gecko.driver", "/Users/admin/Desktop/capmation/selenium/drivers/geckodriver");
 		
-		// the webdriver manager automatically gets
-		// the drivers. for chrome, safari,
-		// change firefoxdriver to chromedriver / safaridriver
-		WebDriverManager.chromedriver().setup();
-//		FirefoxDriver driver = new FirefoxDriver();
-		WebDriver driver = new ChromeDriver();
+		if(browser.equals("chrome")) {
+			// the webdriver manager automatically gets
+			// the drivers. for chrome, safari,
+			// change firefoxdriver to chromedriver / safaridriver
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			
+		} else if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} 
+		
 		driver.get("http://google.com");
 		String title = driver.getTitle();
 		System.out.println(title);
