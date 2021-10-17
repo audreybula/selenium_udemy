dd8d3818f7ca22aaa4b3b37cdf2ad681dc8fb20b
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-17 22:59:05 +1200

    maximize window and implicit wait

--- src/test/java/testcases/TestFindingElements.java
+++ src/test/java/testcases/TestFindingElements.java
@@ -17,7 +17,7 @@ public class TestFindingElements {
 //		WebElement nextBtn = driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span"));
 //		nextBtn.click();
 		
-		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("tflrunningsheet@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("olly.bula@gmail.com");
 		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span")).click();
 	}
 
