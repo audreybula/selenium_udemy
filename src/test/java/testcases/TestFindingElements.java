9a3bd77adc0f64edd84d0b353dff7c7d15f21c64
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-17 17:24:46 +1200

    not necessary to create web elements

--- src/test/java/testcases/TestFindingElements.java
+++ src/test/java/testcases/TestFindingElements.java
@@ -2,19 +2,23 @@ package testcases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestFindingElements {
 	
 	public static void main(String[] args) {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://gmail.com");
-		WebElement username = driver.findElement(By.id("identifierId"));
-		username.sendKeys("olly.bula@gmail.com");
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://gmail.com");
+//		WebElement username = driver.findElement(By.id("identifierId"));
+//		username.sendKeys("tflrunningsheet@gmail.com");
+//		WebElement nextBtn = driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span"));
+//		nextBtn.click();
+		
+		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("tflrunningsheet@gmail.com");
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span")).click();
 	}
 
 }
