65e85c54a96b18492bd9db135f6f64255412838c
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-18 21:06:22 +1200

    select element

--- src/test/java/testcases/TestDropdowns.java
+++ src/test/java/testcases/TestDropdowns.java
@@ -4,7 +4,9 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -13,10 +15,15 @@ public class TestDropdowns {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.way2automation.com/way2auto_jquery/index.php");
+		driver.get("https://www.wikipedia.org");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.findElement(By.name("country")).sendKeys("Fiji");
+		//driver.findElement(By.name("country")).sendKeys("Fiji");
+		WebElement dropdown = driver.findElement(By.id("searchLanguage"));
+		Select select = new Select(dropdown);
+		
+//		select.selectByVisibleText("Eesti");
+		select.selectByValue("hi");
 	}
 
 }
