afbdde5f5e37b7c0abcf9d5664fa53c01a72a63b
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-20 11:34:41 +1200

    checkbox

---
+++ src/test/java/testcases/TestCheckboxes.java
@@ -0,0 +1,27 @@
+package testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestCheckboxes {
+	
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://www.tizag.com/htmlT/htmlcheckboxes.php");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.findElement(By.xpath("/html/body/table[3]/tbody/tr[1]/td[2]/table/tbody/tr/td/div[4]/input[1]")).click();
+
+	}
+
+}
