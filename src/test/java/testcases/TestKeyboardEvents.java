e2c4e72f74ff33e950d51edd037264521fb6e7b3
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-25 15:42:29 +1200

    keyboard events - via actions & directly on sendkeys

--- src/test/java/testcases/TestKeyboardEvents.java
+++ src/test/java/testcases/TestKeyboardEvents.java
@@ -3,28 +3,30 @@ package testcases;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TestRightClick {
+public class TestKeyboardEvents {
 
 	public static void main(String[] args) {
 		
 		WebDriverManager.firefoxdriver().setup();
 		WebDriver driver = new FirefoxDriver();
-		driver.get("http://deluxe-menu,com/popup-mode-sample.html");
+		driver.get("https://www.gmail.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
-		WebElement img = driver.findElement(By.xpath("//img[@src='data-samples/images/popup_pic.gif']"));
+		driver.findElement(By.id("identifierId")).sendKeys("tflrunningsheet@gmail.com");
 		
-		Actions action = new Actions(driver);
-		action.contextClick(img).perform();
+		driver.findElement(By.id("identifierId")).sendKeys(Keys.ENTER);
 		
+//		Actions action = new Actions(driver);
+//		action.sendKeys(Keys.ENTER).perform();
+
 	}
 
 }
