dcef00f4d5291cac030473382bb1e84f290781c3
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-25 23:37:51 +1200

    iframes

---
+++ src/test/java/testcases/TestTryIt.java
@@ -0,0 +1,38 @@
+package testcases;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestTryIt {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_submit_get");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.switchTo().frame("iframeResult");
+		driver.findElement(By.xpath("//button[@onclick='myFunction()']")).click();
+		
+		driver.switchTo().defaultContent();
+		
+		List<WebElement> frames = driver.findElements(By.tagName("iframe"));
+		
+		System.out.println(frames.size());
+		
+		for(WebElement frame : frames) {
+			System.out.println(frame.getAttribute("id"));
+		}
+
+	}
+
+}
