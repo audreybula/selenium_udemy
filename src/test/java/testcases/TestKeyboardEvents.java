608d93c166dc24877a2a81976c3090411f9a0563
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-25 17:42:31 +1200

    multiple keys

--- src/test/java/testcases/TestKeyboardEvents.java
+++ src/test/java/testcases/TestKeyboardEvents.java
@@ -16,16 +16,20 @@ public class TestKeyboardEvents {
 		
 		WebDriverManager.firefoxdriver().setup();
 		WebDriver driver = new FirefoxDriver();
-		driver.get("https://www.gmail.com");
+		driver.get("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 		driver.findElement(By.id("identifierId")).sendKeys("tflrunningsheet@gmail.com");
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div")).click();
 		
-		driver.findElement(By.id("identifierId")).sendKeys(Keys.ENTER);
+		Actions action = new Actions(driver);
+		action.sendKeys(Keys.chord(Keys.COMMAND, "A")).build().perform();
+		action.sendKeys(Keys.chord(Keys.COMMAND+"C")).perform();
 		
-//		Actions action = new Actions(driver);
-//		action.sendKeys(Keys.ENTER).perform();
+		driver.findElement(By.id("identifierId")).click();
+		
+		action.sendKeys(Keys.chord(Keys.COMMAND+"V")).perform();
 
 	}
 
