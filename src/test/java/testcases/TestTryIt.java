26f465cce5aff929cbf890bb2e3e08fdfa527b97
Olivia Bula <46657101+audreybula@users.noreply.github.com>
2021-10-27 11:07:51 +1200

    screenshot utility function

--- src/test/java/testcases/TestTryIt.java
+++ src/test/java/testcases/TestTryIt.java
@@ -3,6 +3,7 @@ package testcases;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -18,38 +19,47 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestTryIt {
 
+	public static WebDriver driver;
+
+	public static void captureScreenshot() throws IOException {
+
+		Date d = new Date();
+		String fileName = d.toString().replace(":", "_").replace(" ", "_") + ".jpg";
+
+		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshot, new File("/Users/admin/Desktop/dev/selenium/selenium_udemy/src/screenshot/" + fileName));
+
+	}
+
 	public static void main(String[] args) throws IOException {
-		
+
 		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
+		driver = new FirefoxDriver();
 		driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_submit_get");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
+
 		driver.switchTo().frame("iframeResult");
 //		driver.findElement(By.xpath("//button[@onclick='myFunction()']")).click();
-		
+
 //		((JavascriptExecutor) driver).executeScript("myFunction()");
-		
-		
-		
+
 		WebElement elem = driver.findElement(By.xpath("//input[@id='mySubmit']"));
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("myFunction()");
 		js.executeScript("arguments[0].style.border='3px solid red'", elem);
-		
+
 		driver.switchTo().defaultContent();
-		
+
 		List<WebElement> frames = driver.findElements(By.tagName("iframe"));
-		
+
 		System.out.println(frames.size());
-		
-		for(WebElement frame : frames) {
+
+		for (WebElement frame : frames) {
 			System.out.println(frame.getAttribute("id"));
 		}
-		
-		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(screenshot, new File("/Users/admin/Desktop/dev/selenium/selenium_udemy/src/screenshot/img.jpg"));
+
+		captureScreenshot();
 
 	}
 
